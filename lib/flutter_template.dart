3132e63b84e1ba3c00985e58fcc7b28e3beb64c9
mzaka <mzaka1945@gmail.com>
2023-01-17 14:12:51 +0700

    testing

--- lib/flutter_template.dart
+++ lib/flutter_template.dart
@@ -1,7 +1,30 @@
 library flutter_template;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomButton extends StatelessWidget {
+  VoidCallback onPressed;
+  ButtonStyle? style;
+  final Widget child;
+  CustomButton(
+      {Key? key, required this.onPressed, required this.child, this.style})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: onPressed,
+        style: style ??
+            TextButton.styleFrom(
+              padding: const EdgeInsets.all(16.0),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.red,
+              elevation: 9.0,
+              textStyle: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+        child: child);
+  }
 }
